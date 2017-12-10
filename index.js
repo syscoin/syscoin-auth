5ef73e317ee97ad39ed51707b0f9d76b76bc95e0
Sebastian Schepis <sschepis@gmail.com>
2017-12-10 13:08:28 -0700

    added sys mainnet and testnet

--- index.js
+++ index.js
@@ -3,6 +3,28 @@
 const bitcore = require('bitcore-lib');
 const Message = require('bitcore-message');
 
+bitcore.Networks.add({
+  name: 'sys_livenet',
+  alias: 'sys_mainnet',
+  pubkeyhash: 0x3F,
+  privatekey: 0x80,
+  scripthash: 0x05,
+  xpubkey: 0x0488b21e,
+  xprivkey: 0x0488ade4,
+  networkMagic: 0xf9beb4d9,
+  port: 8369
+});
+
+bitcore.Networks.add({
+  name: 'sys_testnet',
+  alias: 'sys_testnet',
+  pubkeyhash: 0x41,
+  privatekey: 0xef,
+  scripthash: 0xc4,
+  xpubkey: 0x043587cf,
+  xprivkey: 0x04358394
+});
+
 module.exports.hashPayload = function(payload, privkey) {
   const value = new Buffer(payload);
   const hash = bitcore.crypto.Hash.sha256(value).toString('hex');
--- test.js
+++ test.js
@@ -4,8 +4,11 @@ const syscoinAuth = require('./index');
 var privateKeyWIF = 'L23PpjkBQqpAF4vbMHNfTZAb3KFPBSawQ7KinFTzz7dxq6TZX8UA';
 var privateKey = new bitcore.PrivateKey(privateKeyWIF);
 var address = privateKey.toAddress();
+var addressSys = privateKey.toAddress('sys_livenet');
 
 var payload = 'This is a test payload';
+
+console.log(address);
 
 var authData = syscoinAuth.hashPayload(payload, privateKeyWIF);
 
@@ -16,5 +19,9 @@ var hashVerified = syscoinAuth.verifyHash(payload, authData.hash);
 console.log(hashVerified);
 
 var sigVerified = syscoinAuth.verifySignature(authData.hash,authData.signedHash, address);
+
+console.log(sigVerified);
+
+sigVerified = syscoinAuth.verifySignature(authData.hash,authData.signedHash, addressSys);
 
 console.log(sigVerified);
